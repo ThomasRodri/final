3abffcffb5c37a7cd8596c54a2a02b9a42a0c87d
ThomasRodri <thomas04.rodrigues04@gmail.com>
2024-10-22 16:40:24 -0300

    ajustes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 import Link from "next/link";
-import Cabecalho from "./components/Cabecalho";
-import InformacaoBot from "./components/InformacaoBot";
-
-import Rodape from "./components/Rodape";
 
 
 export default function Home() {
- return(<main className="">
- <h1>Home</h1>
-<Link href="/Home">Home 2</Link>
-
- </main>
+  return(
+  <div>
+    <h1>Home</h1> <Link href="/Home">Home 2</Link>
+    
+  </div>
       
   );
 }
