e7569f6640fa311216b0ed7fe58687d1bab2a5de
ThomasRodri <thomas04.rodrigues04@gmail.com>
2024-10-22 16:41:22 -0300

    botao cadastro

---
+++ src/app/components/ButtonsCadastro.tsx
@@ -0,0 +1,25 @@
+"use client"
+import React from 'react';
+
+export const ButtonsCadastro: React.FC = () => {
+  return (
+    <>
+      {/* Submit Button */}
+      <button
+        type="submit"
+        className="w-full px-5 py-2 bg-blue-500 text-white rounded-md mt-4 cursor-pointer transition-colors duration-300 hover:bg-blue-700"
+      >
+        Enviar as Informações
+      </button>
+
+      {/* Back Button */}
+      <button
+        type="button"
+        className="w-full px-5 py-2 bg-gray-600 text-white rounded-md mt-3 cursor-pointer transition-colors duration-300 hover:bg-gray-700"
+        onClick={() => window.location.href = '/login'}
+      >
+        Voltar
+      </button>
+    </>
+  );
+};
