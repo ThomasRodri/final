a8a89e18ac768c3b34e286e97415c9094a2f6864
ThomasRodri <thomas04.rodrigues04@gmail.com>
2024-11-04 19:28:37 -0300

    botao para a pagina integrantes

--- src/app/components/Rodape.tsx
+++ src/app/components/Rodape.tsx
@@ -1,12 +1,16 @@
-"use client"
+"use client";
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
-import logo from './../../public/images/portobranco.png'
+import logo from './../../public/images/portobranco.png';
 
 const Rodape: React.FC = () => {
   return (
     <footer className="p-5 bg-black text-white text-center">
-      <Image src={logo} alt='Logo Porto' className='w-28 mx-auto' />
+      <Image src={logo} alt="Logo Porto" className="w-28 mx-auto mb-4" />
+      <Link href="/Integrantes" className="inline-block bg-white text-black py-2 px-4 rounded hover:bg-black hover:text-white transition-colors">
+        Ir para a página de Integrantes
+      </Link>
     </footer>
   );
 };
